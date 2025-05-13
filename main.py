782090fa82c7d3fda949b6fc8217c61a20da29b8
Vasile Marin <vasilemarin93@gmail.com>
2025-05-13 19:47:35 +0100

    render player on black screen. rotate player left-right
    CircleShape inherits from pygame Sprite class. takes x, y coords and radius
    
    Player inherits from CircleShape. takes in x and y coords. Defines a triangle and uses pygame draw to draw it. Rotate changes the direction the triangle points to. Update listens to key press(a & d) and calls rotate.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
