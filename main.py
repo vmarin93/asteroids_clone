cf26c2f4f09be9ebaf14aa85a97398820e995d60
Vasile Marin <vasilemarin93@gmail.com>
2025-05-13 17:11:38 +0100

    pygame.init(), set screen size, first game loop(opens black screen)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
